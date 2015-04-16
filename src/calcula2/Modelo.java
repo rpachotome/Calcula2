a5d35e24e8da2abba58f43d6be7f94458593f5ab
rpachotome <rpachotome@atanasoft01.danielcastelao.org>
2015-04-16 09:35:11 +0200

    primer commit

---
+++ src/calcula2/Modelo.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calcula2;
+
+/**
+ *
+ * @author rpachotome
+ */
+public class Modelo {
+    private int num1;
+    private int num2;
+    private String Operacion;
+    private int resultado;
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public String getOperacion() {
+        return Operacion;
+    }
+
+    public void setOperacion(String Operacion) {
+        this.Operacion = Operacion;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
+    }
+    
+    
+}
